8e3ffcbc22c66eb06e6b4542548a1ba1861d2447
Dmitry-dms <dmitry.cbip@gmail.com>
2022-06-18 17:15:45 +0500

    s3

--- pkg/gogl/texture.go
+++ pkg/gogl/texture.go
@@ -1,22 +1,22 @@
 package gogl
 
 import (
-	// "encoding/json"
 	"image"
 	"image/color"
 	"image/draw"
 	"image/png"
 	_ "image/png"
+	_ "image/jpeg"
 	"os"
 
 	"github.com/go-gl/gl/v4.2-core/gl"
 )
 
 type Texture struct {
-	filepath  string  `json:"filepath"`
-	textureId uint32  `json:"texture_id"`
-	width     int32 `json:"texture_width"`
-	height    int32 `json:"texture_height"`
+	Filepath  string  `json:"filepath"`
+	TextureId uint32  `json:"texture_id"`
+	Width     int32 `json:"texture_width"`
+	Height    int32 `json:"texture_height"`
 }
 
 type TextureExported struct {
@@ -27,95 +27,19 @@ type TextureExported struct {
 }
 
 func (t *Texture) GetFilepath() string {
-	return t.filepath
+	return t.Filepath
 }
 
 func CreateTexture(filepath string, texId uint32, width, height int32) *Texture {
 	t := &Texture{
-		filepath: filepath,
-		width: width,
-		height: height,
-		textureId: texId,
+		Filepath: filepath,
+		Width: width,
+		Height: height,
+		TextureId: texId,
 	}
 	// t.Init(filepath)
 	return t
 }
-
-
-
-func (t *Texture) Init(filepath string) (*Texture, error) {
-	infile, err := os.Open(filepath)
-	if err != nil {
-		return nil, err
-	}
-	defer infile.Close()
-
-	img, _, err := image.Decode(infile)
-	if err != nil {
-		return nil, err
-	}
-
-	w := img.Bounds().Max.X
-	h := img.Bounds().Max.Y
-
-	pixels := make([]byte, w*h*4)
-	bIndex := 0
-	for y := 0; y < h; y++ {
-		for x := 0; x < w; x++ {
-			r, g, b, a := img.At(x, y).RGBA()
-			pixels[bIndex] = byte(r / 256)
-			bIndex++
-			pixels[bIndex] = byte(g / 256)
-			bIndex++
-			pixels[bIndex] = byte(b / 256)
-			bIndex++
-			pixels[bIndex] = byte(a / 256)
-			bIndex++
-		}
-	}
-
-	texture := genBindTexture()
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
-	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, int32(w), int32(h), 0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(pixels))
-	//gl.GenerateMipmap(gl.TEXTURE_2D)
-
-	textureStruct := Texture{
-		filepath:  filepath,
-		textureId: texture,
-		width:     int32(w),
-		height:    int32(h),
-	}
-	return &textureStruct, nil
-}
-
-func genBindTexture() uint32 {
-	var texId uint32
-	gl.GenTextures(1, &texId)
-	gl.BindTexture(gl.TEXTURE_2D, texId)
-	return texId
-}
-
-func (t *Texture) Bind() {
-	gl.BindTexture(gl.TEXTURE_2D, t.textureId)
-}
-func (t *Texture) Unbind() {
-	gl.BindTexture(gl.TEXTURE_2D, 0)
-}
-
-func (t *Texture) GetWidth() int32 {
-	return t.width
-}
-func (t *Texture) GetHeight() int32 {
-	return t.height
-}
-
-func (t *Texture) GetId() uint32 {
-	return t.textureId
-}
-
 
 func NewTextureFramebuffer(width, height int32) *Texture {
 	texture := genBindTexture()
@@ -126,13 +50,40 @@ func NewTextureFramebuffer(width, height int32) *Texture {
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
 
 	textureStruct := Texture{
-		filepath:  "generated",
-		textureId: texture,
-		width:     width,
-		height:    height,
+		Filepath:  "generated",
+		TextureId: texture,
+		Width:     width,
+		Height:    height,
 	}
 	return &textureStruct
 }
+
+
+func genBindTexture() uint32 {
+	var texId uint32
+	gl.GenTextures(1, &texId)
+	gl.BindTexture(gl.TEXTURE_2D, texId)
+	return texId
+}
+
+func (t *Texture) Bind() {
+	gl.BindTexture(gl.TEXTURE_2D, t.TextureId)
+}
+func (t *Texture) Unbind() {
+	gl.BindTexture(gl.TEXTURE_2D, 0)
+}
+
+func (t *Texture) GetWidth() int32 {
+	return t.Width
+}
+func (t *Texture) GetHeight() int32 {
+	return t.Height
+}
+
+func (t *Texture) GetId() uint32 {
+	return t.TextureId
+}
+
 
 
 func UploadTextureFromMemory(data *image.Gray) *Texture {
@@ -158,7 +109,6 @@ func UploadTextureFromMemory(data *image.Gray) *Texture {
 	}
 
 	texture := genBindTexture()
-
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
 	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
@@ -166,11 +116,10 @@ func UploadTextureFromMemory(data *image.Gray) *Texture {
 	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA, int32(w), int32(h), 0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(pixels))
 	// gl.GenerateMipmap(gl.TEXTURE_2D)
 
-// gl.BindTexture(gl.TEXTURE_2D, 0)
 	textureStruct := Texture{
-		textureId: texture,
-		width:     int32(w),
-		height:    int32(h),
+		TextureId: texture,
+		Width:     int32(w),
+		Height:    int32(h),
 	}
 	return &textureStruct
 }
@@ -216,14 +165,74 @@ func TextureFromPNG(filepath string) (*Texture, error) {
 	//gl.GenerateMipmap(gl.TEXTURE_2D)
 
 	textureStruct := Texture{
-		filepath:  filepath,
-		textureId: texture,
-		width:     int32(w),
-		height:    int32(h),
+		Filepath:  filepath,
+		TextureId: texture,
+		Width:     int32(w),
+		Height:    int32(h),
 	}
 	return &textureStruct, nil
 }
 
+func (t *Texture) Init(filepath string) (*Texture, error) {
+	infile, err := os.Open(filepath)
+	if err != nil {
+		return nil, err
+	}
+	defer infile.Close()
+
+	img, _, err := image.Decode(infile)
+	if err != nil {
+		return nil, err
+	}
+
+	w := img.Bounds().Dx()
+	h := img.Bounds().Dy()
+
+	pixels := make([]byte, w*h*4)
+	// bIndex := 0
+	// for y := 0; y < h; y++ {
+	// 	for x := 0; x < w; x++ {
+	// 		r, g, b, a := img.At(x, y).RGBA()
+	// 		pixels[bIndex] = byte(r / 256)
+	// 		bIndex++
+	// 		pixels[bIndex] = byte(g / 256)
+	// 		bIndex++
+	// 		pixels[bIndex] = byte(b / 256)
+	// 		bIndex++
+	// 		pixels[bIndex] = byte(a / 256)
+	// 		bIndex++
+	// 	}
+	// }
+	i := 0
+	for y := img.Bounds().Dy() - 1; y >= 0; y-- {
+		for x := 0; x < img.Bounds().Dx(); x++ {
+			c := color.NRGBAModel.Convert(img.At(x, y)).(color.NRGBA)
+			pixels[i] = c.R
+			pixels[i+1] = c.G
+			pixels[i+2] = c.B
+			pixels[i+3] = c.A
+
+			i += 4
+		}
+	}
+
+	texture := genBindTexture()
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+
+	gl.TexImage2D(gl.TEXTURE_2D, 0, gl.RGBA8, int32(w), int32(h), 0, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(pixels))
+	// gl.GenerateMipmap(gl.TEXTURE_2D)
+
+	textureStruct := Texture{
+		Filepath:  filepath,
+		TextureId: texture,
+		Width:     int32(w),
+		Height:    int32(h),
+	}
+	return &textureStruct, nil
+}
 
 
 
@@ -289,3 +298,4 @@ func imageToBytesSlow(img image.Image) []byte {
 	draw.Draw(dstImg, image.Rect(0, 0, w, h), img, img.Bounds().Min, draw.Src)
 	return bs
 }
+
